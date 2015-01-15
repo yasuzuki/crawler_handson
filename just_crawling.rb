d171f033abdeeaf0685e6d4cf8182810ff1f9469
Yasunori Suzuki <railsyas@gmail.com>
2015-01-15 23:03:58 +0900

    use local proxy.

--- just_crawling.rb
+++ just_crawling.rb
@@ -5,7 +5,14 @@ urls = [
   "http://www.amazon.co.jp/gp/bestsellers/digitall-text/2275256051"
 ]
 
-Anemone.crawl(urls, depth_limit: 1, skip_query_strings: true) do |anemone|
+options = {
+  depth_limit: 1,
+  skip_query_strings: true,
+  proxy_host: 'localhost',
+  proxy_port: '5432',
+}
+
+Anemone.crawl(urls,options) do |anemone|
 
   anemone.focus_crawl do |page|
     page.links.keep_if do |link|
@@ -13,7 +20,8 @@ Anemone.crawl(urls, depth_limit: 1, skip_query_strings: true) do |anemone|
     end
   end
 
-  anemone.on_every_page do |page|
+  PATTERN = /466298\/+|466282\/+|2291657051\/+|2291905051\/+/
+  anemone.on_pages_like(PATTERN) do |page|
     puts page.url
   end
 end
